637a8c0e6a07577657d2f9baff1c3810fc036429
David Lechner <david@lechnology.com>
2017-01-23 00:11:17 -0600

    Fix maintainer release script

--- release.sh
+++ release.sh
@@ -16,4 +16,6 @@ gbp buildpackage --git-tag-only
 ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedsc jessie \
     ~/reprepro/debian/pool/main/${source:0:1}/${source}/${source}_${version}.dsc"
 ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb jessie \
-    ~/reprepro/debian/pool/main/${source:0:1}/${source}/ev3dev-tools_${version}_all.deb"
+    ~/reprepro/debian/pool/main/${source:0:1}/${source}/python-ptvsd_${version}_all.deb"
+ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb jessie \
+    ~/reprepro/debian/pool/main/${source:0:1}/${source}/python3-ptvsd_${version}_all.deb"
