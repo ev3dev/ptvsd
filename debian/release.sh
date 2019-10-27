e4dc8a7903fae462348b73224096cbe4d3e18147
David Lechner <david@lechnology.com>
2019-10-27 16:05:01 -0500

    debian: drop use of codename in release script
    server changed, so we need to use codename as distribution

--- debian/release.sh
+++ debian/release.sh
@@ -9,17 +9,16 @@ export SKIP_CYTHON_BUILD=1
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
-codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
-debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+OS=debian DIST=${distribution} ARCH=amd64 pbuilder-ev3dev build
+debsign ~/pbuilder-ev3dev/debian/${distribution}-amd64/${source}_${version}_amd64.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${distribution}-amd64/${source}_${version}_amd64.changes
 
 gbp buildpackage --git-tag-only
 
-ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedsc ${codename} \
+ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedsc ${distribution} \
     ~/reprepro/debian/pool/main/${source:0:1}/${source}/${source}_${version}.dsc"
-ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb ${codename} \
+ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb ${distribution} \
     ~/reprepro/debian/pool/main/${source:0:1}/${source}/python-ptvsd_${version}_all.deb"
-ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb ${codename} \
+ssh ev3dev@reprepro.ev3dev.org "reprepro -b ~/reprepro/raspbian includedeb ${distribution} \
     ~/reprepro/debian/pool/main/${source:0:1}/${source}/python3-ptvsd_${version}_all.deb"
